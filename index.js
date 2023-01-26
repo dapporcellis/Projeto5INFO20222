4bc43393b719edb8da4471140590c132368a1bf5
Diego de Abreu Porcellis <dapporcellis@hotmail.com>
2023-01-26 14:29:54 -0300

    Completo

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const app = express();
 const path = require("path");
 
 app.use(express.static(path.join(__dirname, "public")));
-
 app.use(express.urlencoded({ extended: true }));
-
 app.set("view engine", "ejs");
 
 const usuarioRoute = require("./routes/usuarioRoute")
